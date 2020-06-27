a59a8c6746e6ae275042475b775322d28cd12a73
agneshew <agnes.hewusz@gmail.com>
2020-06-27 22:53:20 +0200

    First commit

---
+++ src/main/java/com/example/Calculator.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class Calculator {
+
+    public void add() {
+    }
+    public void subtraction() {
+    }
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.add();
+        calculator.subtraction();
+        System.out.println("It works!");
+    }
+}
