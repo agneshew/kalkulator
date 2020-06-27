1901a24621ea8c9c40a73ea4dbec22365bd2ef41
agneshew <agnes.hewusz@gmail.com>
2020-06-27 23:01:26 +0200

    implemented functionality

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -2,14 +2,16 @@ package com.example;
 
 public class Calculator {
 
-    public void add() {
+    public void addAToB(double a, double b) {
+        System.out.println(a + b);
     }
-    public void subtraction() {
+    public double subtractionAFromB(double a, double b) {
+        return b - a;
     }
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        calculator.add();
-        calculator.subtraction();
+        calculator.addAToB(5, 23);
+        System.out.println(calculator.subtractionAFromB(456.678, 12.234));
         System.out.println("It works!");
     }
 }
